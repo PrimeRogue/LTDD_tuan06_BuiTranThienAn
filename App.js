d9dc60da3015de3904fa2e4b8e54c8b77a92a748
Bui Tran Thien An <buitranthienan1111@gmail.com>
2023-10-12 20:00:49 +0700

    order Component

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import { StatusBar } from "expo-status-bar";
 import Login from "./components/login";
 import AddImage from "./components/addImage";
 import PasswordGenerator from "./components/passwordGenerator";
+import Order from "./components/order";
 
 export default function App() {
   // return <Login></Login>;
   // return <AddImage></AddImage>;
-  return <PasswordGenerator></PasswordGenerator>;
+  // return <PasswordGenerator></PasswordGenerator>;
+  return <Order></Order>;
 }
