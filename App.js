27fcd83b7a57ece7b1f5a159994b499907c655bb
Bui Tran Thien An <buitranthienan1111@gmail.com>
2023-10-12 19:54:34 +0700

    passwordGenerator Component

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import Login from "./components/login";
 import AddImage from "./components/addImage";
+import PasswordGenerator from "./components/passwordGenerator";
 
 export default function App() {
   // return <Login></Login>;
-  return <AddImage></AddImage>;
+  // return <AddImage></AddImage>;
+  return <PasswordGenerator></PasswordGenerator>;
 }
